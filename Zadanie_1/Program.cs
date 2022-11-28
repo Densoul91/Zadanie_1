29a7629c8beee925bf242c1da7f2a297233fe4cd
Denis <Denis@DESKTOP-R5EKC9J>
2022-11-28 11:51:48 +0200

    Добавьте файлы проекта.

---
+++ Zadanie_1/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Добрый день!\nВведите ваши данные:");
+
+        Console.Write("\n1. Имя: ");
+        var name = Console.ReadLine();
+
+        Console.Write("2. Фамилия: ");
+        var lastname = Console.ReadLine();
+
+        Console.Write("3. Отчество: ");
+        var patronymic = Console.ReadLine();
+
+        Console.Write("4. Ваш email: ");
+        var mail = Console.ReadLine();
+
+        Console.Write("5. Номер телефона: ");
+        var phone = Console.ReadLine();
+
+        Console.Write("6. Фирма, в которой работаете: ");
+        var job = Console.ReadLine();
+
+        Console.WriteLine("\nПроверьте введеные данные:\n");
+        Console.Write($"{lastname} {name} {patronymic}\nТелефон: {phone}\nEmail: {mail}\nРаботает в {job}");
+    }
+}
